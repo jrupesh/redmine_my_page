153ffd52cc3e974efbd9ffd03e5590ee36256e70
Rupesh J <j.rupesh@gmail.com>
2015-11-19 18:00:51 +0530

    Update in compliance with the dashboard plugin.

--- lib/my_page_patches/my_controller_patch.rb
+++ lib/my_page_patches/my_controller_patch.rb
@@ -19,7 +19,20 @@ module MyPagePatches
     module InstanceMethods
       def my_custom_form
         @user = User.current
+        @object = params[:object]
         @pref = @user.pref
+
+        if @object == 'dashboard'
+          @dashboard = Dashboard.find_by_id(params[:dashboard_id])
+          deny_access unless @dashboard && @dashboard.manage_layout?(@user)
+        end
+        if params["type"].present? && params["type"] == 'my_cust_query'
+          @vartype = "my_cust_query"
+          @my_cust_query = @object == 'dashboard' ? @dashboard.my_cust_query : @pref.my_cust_query
+        else
+          @vartype = "my_activity"
+          @my_cust_query = @object == 'dashboard' ? @dashboard.my_activity : @pref.my_activity
+        end
 
         visible_queries_array = if IssueQuery.respond_to? (:esi_visible_queries)
           IssueQuery.esi_visible_queries.
@@ -31,28 +44,37 @@ module MyPagePatches
             pluck(:name, :id, "projects.name").to_a
         end
         @visible_queries = visible_queries_array.collect { |name, id, projectname| ["#{projectname.blank? ? "" : projectname + " - "}#{name}", id ] }
-
-        if params["type"].present? && params["type"] == 'my_cust_query'
-          @vartype = "my_cust_query"
-          @my_cust_query = @pref.my_cust_query
-        else
-          @vartype = "my_activity"
-          @my_cust_query = @pref.my_activity
-        end
       end
 
       def update_queries
-        if params["my_cust_query"].present?
-          @user_pref              = User.current.pref.my_cust_query
-          @user_pref[:limit]      = params["my_cust_query"]["limit"] || 10
-          @user_pref[:query_ids]  = params["my_cust_query"]["query_ids"].any? ? params["my_cust_query"]["query_ids"].collect { |i| i.to_i } : []
-          User.current.pref.save
-        elsif params["my_activity"].present?
-          @user_pref              = User.current.pref.my_activity
-          @user_pref[:query_ids]  = params["my_activity"]["query_ids"].any? ? params["my_activity"]["query_ids"].collect { |i| i.to_i } : []
-          User.current.pref.save
+        @object = params[:object]
+        if @object == 'dashboard'
+          @dashboard              = Dashboard.find_by_id(params[:dashboard_id])
+          deny_access unless @dashboard && @dashboard.manage_layout?(User.current)
+          if params["my_cust_query"].present?
+            dashboard_pref              = @dashboard.my_cust_query
+            dashboard_pref[:limit]      = params["my_cust_query"]["limit"] || 10
+            dashboard_pref[:query_ids]  = params["my_cust_query"]["query_ids"].any? ? params["my_cust_query"]["query_ids"].collect { |i| i.to_i } : []
+            @dashboard.save
+          elsif params["my_activity"].present?
+            dashboard_pref              = @dashboard.my_activity
+            dashboard_pref[:query_ids]  = params["my_activity"]["query_ids"].any? ? params["my_activity"]["query_ids"].collect { |i| i.to_i } : []
+            @dashboard.save
+          end
+          redirect_to dashboards_path( :id => @dashboard.id )
+        else
+          if params["my_cust_query"].present?
+            @user_pref              = User.current.pref.my_cust_query
+            @user_pref[:limit]      = params["my_cust_query"]["limit"] || 10
+            @user_pref[:query_ids]  = params["my_cust_query"]["query_ids"].any? ? params["my_cust_query"]["query_ids"].collect { |i| i.to_i } : []
+            User.current.pref.save
+          elsif params["my_activity"].present?
+            @user_pref              = User.current.pref.my_activity
+            @user_pref[:query_ids]  = params["my_activity"]["query_ids"].any? ? params["my_activity"]["query_ids"].collect { |i| i.to_i } : []
+            User.current.pref.save
+          end
+          redirect_to my_page_path
         end
-        redirect_to my_page_path
       end
     end
   end
