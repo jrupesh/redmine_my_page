d8b3c10404c4c4e5d7313cd2b1bb424e4d857e38
Rupesh J <j.rupesh@gmail.com>
2017-10-18 00:34:34 +0530

    Redmine 3.4.2 Ongoing

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,2 @@
-get '/my/my_custom_form', :to => 'my#my_custom_form', :as => 'myqueryform'
+get '/my/my_custom_form', :to => 'my#my_custom_form', :as => 'mycustomqueryform'
 post '/my/update_queries', :to => 'my#update_queries', :as => 'updatemyquery'
