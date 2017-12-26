eaedb4fd86a3d85749fcb8a1593242a01cc5a745
will.chou <will.chou@superlinksoft.com>
2017-12-26 22:42:13 +0800

    1. add locale label_my_landing_page in zh.yml 2. integrate with dashboard plugin

--- lib/mypage_helper.rb
+++ lib/mypage_helper.rb
@@ -13,7 +13,10 @@ module MypageHelper
       return if query.nil?
       param_hash = query.project_id.nil? ? { :query_id => query_id } : { :project_id => query.project_id, :query_id => query_id }
       ret_url = view.issues_url( param_hash )
-    elsif Redmine::Plugin.installed?(:redmine_agile)
+    elsif pref.landing_page.start_with?('my_page')
+      ret_url = view.my_page_url
+    end
+    if Redmine::Plugin.installed?(:redmine_agile)
       if pref.landing_page.start_with?('ap-')
         home_project = Project.find_by_id(pref.landing_page.gsub("ap-","").to_i)
         return if home_project.nil? || home_project.archived?
@@ -25,8 +28,13 @@ module MypageHelper
         param_hash = query.project_id.nil? ? { :query_id => query_id } : { :project_id => query.project_id, :query_id => query_id }
         ret_url = view.agile_board_url( param_hash )
       end
-    elsif pref.landing_page.start_with?('my_page')
-      ret_url = view.my_page_url
+    end
+    if Redmine::Plugin.installed?(:redmine_dashboard)
+      if pref.landing_page.start_with?('rdb-')
+        home_project = Project.find_by_id(pref.landing_page.gsub("rdb-","").to_i)
+        return if home_project.nil? || home_project.archived?
+        ret_url = view.rdb_taskboard_url( :id => home_project.id )
+      end
     end
     ret_url
   end
